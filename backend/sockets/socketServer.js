0b791a7d1f05c3678406395e9bd5c73753e1dff0
Lavanya2804-dev <lavanyasaniboeena@gmail.com>
2026-05-20 22:02:49 +0530

    Fix socket.io production cors

--- backend/sockets/socketServer.js
+++ backend/sockets/socketServer.js
@@ -3,10 +3,20 @@ import { Server } from "socket.io";
 export const startSocket = (server) => {
 
   const io = new Server(server, {
+
     cors: {
-      origin: "http://localhost:5173",
+
+      origin: [
+        "http://localhost:5173",
+        "https://git-code-collaboration-platform.vercel.app"
+      ],
+
+      methods: ["GET", "POST"],
+
       credentials: true
+
     }
+
   });
 
   io.on("connection", (socket) => {
@@ -15,47 +25,60 @@ export const startSocket = (server) => {
 
     // Join personal user room
     socket.on(
-  "joinUserRoom",
-  (userId) => {
+      "joinUserRoom",
+      (userId) => {
 
-    console.log(
-      "JOIN ROOM EVENT RECEIVED:",
-      userId
+        console.log(
+          "JOIN ROOM EVENT RECEIVED:",
+          userId
+        );
+
+        socket.join(
+          userId.toString()
+        );
+
+        console.log(
+          "User joined room:",
+          userId.toString()
+        );
+
+      }
     );
-
-    socket.join(
-      userId.toString()
-    );
-
-    console.log(
-      "User joined room:",
-      userId.toString()
-    );
-
-  }
-);
 
     // Join repository room
-    socket.on("joinRepoRoom", (repoId) => {
+    socket.on(
+      "joinRepoRoom",
+      (repoId) => {
 
-      socket.join(repoId);
+        socket.join(repoId);
 
-      console.log(`Joined repo room: ${repoId}`);
+        console.log(
+          `Joined repo room: ${repoId}`
+        );
 
-    });
+      }
+    );
 
     // Leave repository room
-    socket.on("leaveRepoRoom", (repoId) => {
+    socket.on(
+      "leaveRepoRoom",
+      (repoId) => {
 
-      socket.leave(repoId);
+        socket.leave(repoId);
 
-      console.log(`Left repo room: ${repoId}`);
+        console.log(
+          `Left repo room: ${repoId}`
+        );
 
-    });
+      }
+    );
 
     socket.on("disconnect", () => {
 
-      console.log("User disconnected:", socket.id);
+      console.log(
+        "User disconnected:",
+        socket.id
+      );
 
     });
 
